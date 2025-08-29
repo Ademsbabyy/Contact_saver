c929de6d789c3e0889ce03ca7aae7c9fd4d4c086
Olaosebikan Favour <olaosebikanfavour2002@gmail.com>
2025-08-29 16:04:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,95 @@
-import csv
-from pathlib import Path
+
 from file_ops import save_participant,load_participants
-
-
-
-
-
-""" The file below was written to test the file opps"""
-
-
+from pathlib import Path
+import csv
 csv_file = 'participants.csv'
+workspace = Path("participant_pkg")
 
+print("\tCONTACT SAVER")
+print("You are welcome to the Publica tech workshop\nPlease do well to fill in correct details\nThank You!")
 
-
-
+participant_dict = {}
 while True:
-    name = input("Enter your name: ")
-    if name =="":
-        print("Name cannot be empty")
-    else:
-     phone = input("Enter your phone number: ")
-
-     if not phone.isdigit() :
-        print("The phone number must contain only numbers")
-     elif len(phone) != 11:
-        print("The phone number must be eleven")
-     elif not phone.isdigit() and len(phone) != 11:
-         print("Please enter the right number!")
-     else:
-        track = input("Enter your track: ")
-        if track == "":
-           print("Track cannot be empty!")
+    try: 
+        name = input("What is your name?")
+        if not name.isalpha():
+            raise ValueError("Invalid input")
+        if name == "":
+         print("Name cannot be empty")
+         continue
         else:
-           age = input("Enter your age: ")
-           try:
+         age = input("How Old are you?")
+         try:
               age = int(age)
-           except:
-              print("Your age must be a number!,please try again")
+         except:
+              print("Your age must be a number!\nplease try again")
               age = int(input("Enter your age: "))
-              break
-        break
+              continue
+         else:
+          phone = input("Enter your phone number: ")
+         if not phone.isdigit() :
+             print("The phone number must contain only numbers")
+         elif len(phone) != 11:
+            print("The phone number must be eleven")
+         elif not phone.isdigit() and len(phone) != 11:
+            print("Please enter the right number!")
+            continue
+         else:
+           track = input("Enter your track: ")
+           if track == "":
+            print("Track cannot be empty!")
+           else:
+            print("Valid track")
+        break   
+    except ValueError as e:
+        print("Error:", e)
 
+participant_dict = {name, age, phone, track}
+# participant = { 
+#    "name", "age", "phone", "track",  
+#     name, age, phone,track
+#     }
 
-participant =  {
+participant =  { 
    'Name' : name,
     'Age' : age,
     'Track' : track,
     'Phone' : phone
 
 }
-
-
-
-
-
-
-
-
 save_participant(csv_file,participant)
 all_participants = load_participants(csv_file)
+
+
+# for i in all_participants:
+#      print(i)
+# try:
+#       for index, i in enumerate(all_participants):
+#        if index == 0:  # Header row
+#         print(f"Headers: {' | '.join(i)}")
+#         print("_" * 40)
+
+#        else:
+#         print(i)
+# except :
+#      print("Try again!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
